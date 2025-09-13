8fa77d04f188168fdf227fba556d127cb55cfdf7
Seceng <thosan.girisona@gmail.com>
2025-09-13 08:14:59 +0700

    Add sql UI

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,6 +7,7 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.gnumake
+    pkgs.docker-compose
     # pkgs.python311
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
